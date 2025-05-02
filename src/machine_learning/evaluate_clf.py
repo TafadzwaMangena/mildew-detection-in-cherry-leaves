cd90f96fe30191a604d19d6cb6a780af7aadba69
Tafadzwa Mangena <141625148+TafadzwaMangena@users.noreply.github.com>
2025-05-02 12:23:26 +0000

    Add doctrings to evaluate_clf.py

--- src/machine_learning/evaluate_clf.py
+++ src/machine_learning/evaluate_clf.py
@@ -3,4 +3,7 @@ from src.data_management import load_pkl_file
 
 
 def load_test_evaluation(version):
+    """
+    Load evaluation of ML model on test set
+    """
     return load_pkl_file(f'outputs/{version}/evaluation.pkl')
